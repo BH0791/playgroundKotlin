74dc3414168f9079d508f1b7ef64a4829295ede4
HamidB <ejecteur@yahoo.fr>
2024-01-11 16:25:48 +0100

    Update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,64 +3,70 @@ package fr.hamtec
 fun main() {
 
     //lesVariable()
-    lesTableaux()
-
+    //lesTableaux()
+    lesTableauxEtLesFonctions()
 }
-
-
-private fun lesTableaux(){
+fun displayNames(names: Array<String>){
+    //-Avec la V3
+    for (name in names){
+        println(name)
+    }
+}
+private fun lesTableauxEtLesFonctions(){
+    val names = arrayOf("Sylvie", "Ludovic", "Paul", "Jean")
+    displayNames(names)
+}
+private fun lesTableaux() {
     val names = arrayOf("Sylvie", "Ludovic", "Paul", "Jean")
     val indice = names[2]
     println(names[0] + " $indice")
-
-    for (index in 0..names.size-1){
+//-V1
+    for (index in 0..names.size - 1) {
         print(names[index])
     }
     println()
-    for (index in names.indices){
+//-V2
+    for (index in names.indices) {
         print(names[index])
     }
     println()
-    for (name in names){
+//-V3
+    for (name in names) {
         print(name)
     }
 }
-fun myfun(a: Int, b: Int): Int{
+
+fun myfun(a: Int, b: Int): Int {
     return a + b
 }
+
 private fun minOf(a: Int, b: Int): Int {
     return if (a < b) a else b
 }
-private fun lesVariable(){
-    //    var nombreDeVoiture: Int = 35000
-//    var argent: Long = 1001L
-//    var age: Byte = 100
-//    var prixAspirateur: Short = 123
-//    var prenom: String = "Ludovic"
-//    var estVrai: Boolean = true
-//    var resultatDivision: Double = 0.3456314478541552585211
-//    var resultatSoustraction: Float = 0.365f
-//    var lettre : Char ='A'
-//
-//    var val1: Int = 10
-//    var val2: Int = 20
-    val immutableString: String = "Hello" // Immutable variable
-    var mutableInt: Int = 42 // Mutable variable
+
+fun lesVariable() {
+
+
+    val immutableString: String = "Hello" /* Immutable variable */
+    var mutableInt: Int = 42 /* Mutable variable */
     val inferredType = 42 // Int inferred
     println(immutableString)
-//    for (i in 1..10){
-//        print(i)
-//    }
-//    when (3) {
-//        1 -> print("x == 1")
-//        2 -> print("x == 2")
-//        else -> { // Note the block
-//            print("x is neither 1 nor 2")
-//        }
-//    }
+
+    for (i in 1..10) {
+        print(i)
+    }
+    when (3) {
+        1 -> print("x == 1")
+        2 -> print("x == 2")
+        else -> { // Note the block
+            print("x is neither 1 nor 2")
+        }
+    }
+
 
     println(myfun(10, 20))
     println("La plus petite est = " + minOf(10, 20))
-    var lapp = minOf(15,50)
+    var lapp = minOf(15, 50)
     println("Est = $lapp")
+
 }
