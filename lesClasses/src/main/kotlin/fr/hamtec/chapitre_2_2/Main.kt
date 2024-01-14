c8a7275aca12ff584de3badfb08951aac2e4acc9
HamidB <ejecteur@yahoo.fr>
2024-01-14 15:37:43 +0100

    Update

---
+++ lesClasses/src/main/kotlin/fr/hamtec/chapitre_2_2/Main.kt
@@ -0,0 +1,18 @@
+package fr.hamtec.chapitre_2_2
+
+fun main() {
+    heart()
+    val labradore = Dog()
+    println(labradore)
+}
+
+
+
+
+
+private fun heart() {
+    println()
+    println("******************************************************************")
+    println("******************** Chapitre-2.2 Les classes ********************")
+    println("******************************************************************")
+}
