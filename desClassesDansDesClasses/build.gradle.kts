399be5e985dd3dc0e266c1c6a27c10b44a9b68f7
HamidB <ejecteur@yahoo.fr>
2024-01-24 16:42:32 +0100

    update

---
+++ desClassesDansDesClasses/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "fr.hamtec.bo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
