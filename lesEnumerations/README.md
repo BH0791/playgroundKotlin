2a511b0a4a9656c5d8f2c4f56ef7ef8bb43c446f
HamidB <ejecteur@yahoo.fr>
2024-02-10 17:08:28 +0100

    update

--- lesEnumerations/README.md
+++ lesEnumerations/README.md
@@ -1,1 +1,12 @@
-P161
+## Les énumération
+
+En programmation, il est parfois nécessaire qu'un type n'ait que certaines valeurs. Pour ce faire, le concept d'énumération a été introduit. Une énumération est une liste nommée de constantes.
+En Kotlin, comme dans de nombreux autres langages de programmation, une énumération a son propre type spécialisé, indiquant que quelque chose a un certain nombre de valeurs possibles. Contrairement aux énumérations Java, les énumérations Kotlin sont des classes.
+
+Quelques points importants sur les classes d'enum en Kotlin -
+
+Les constantes d'énumération ne sont pas de simples collections de constantes - elles ont des propriétés, des méthodes, etc.
+Chacune des constantes enum agit comme une instance distincte de la classe et est séparée par des virgules.
+Les enums améliorent la lisibilité de votre code en attribuant des noms prédéfinis aux constantes.
+Une instance de classe enum ne peut pas être créée à l'aide de constructeurs.
+Aujourd'hui, les constantes Enum ne sont pas de simples collections de constantes : elles peuvent avoir des propriétés, implémenter des interfaces et bien plus encore.
--- lesException/src/main/kotlin/Main.kt
+++ lesException/src/main/kotlin/Main.kt
@@ -3,23 +3,46 @@ package fr.hamtec
 fun main() {
 //    instructionMultiCatch()
 //    instructionSimpleCatch()
-    captureException()
+//    captureException()
+//    captureException()
+    println(Dog("Joe").countNameChars())
+    println(Dog("").countNameChars())
 }
 
+private fun countNameCharsV1(dog: Dog): Int {
+    val name = dog.name ?: throw IllegalArgumentException("The name cannot be null_v1")
+    return name.length
+}
+
+private fun countNameCharsV3(dog: Dog): Int {
+    val name = dog.name ?: throw InvalidDogNameException("The name cannot be null_v3")
+    return name.length
+}
+
+private fun countNameCharsV2(dog: Dog): Int =
+    dog.name?.length ?: throw IllegalArgumentException("The name cannot be null_v2")
+
 fun add(a: Int, b: Int): Int {
-    if (a <= 0){
+    if (a <= 0) {
         throw IllegalArgumentException("Message = a <= 0")
-    }else if(b <= 0){
+    } else if (b <= 0) {
         throw IllegalArgumentException("Message = b <= 0")
     }
-        return a + b
+    return a + b
+}
+
+fun addV2(a: Int, b: Int): Int {
+    // ** https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/require.html
+    require(a > 0) { "+++++ a <= 0 +++++" }
+    require(b > 0) { "+++++ b <= 0 +++++" }
+    return a + b
 }
 
 private fun captureException() {
     println("Lever une Exeption")
-    val a: Int = 10
+    val a: Int = -10
     val b: Int = -5
-    println(add(a, b))
+    println(addV2(a, b))
 }
 
 private fun instructionMultiCatch() {
@@ -50,3 +73,13 @@ private fun instructionSimpleCatch() {
         println("Erreur! => ${exe.message}")
     }
 }
+
+data class Dog(val name: String) {
+    fun countNameChars(): Int {
+        check(name.isNotEmpty()) { "The name cannot be empty" }
+        return name.length
+    }
+    fun isNoEmpty(): Boolean {return true}
+}
+
+class InvalidDogNameException(message: String) : Exception(message)
