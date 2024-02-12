7c40385ed0d94b80c608600f97830ce57a20953d
HamidB <ejecteur@yahoo.fr>
2024-02-12 11:15:20 +0100

    update

--- lesCollections/src/main/kotlin/Main.kt
+++ lesCollections/src/main/kotlin/Main.kt
@@ -2,11 +2,12 @@ package fr.hamtec
 
 import java.util.*
 
+
 fun main() {
     println("Hello collection!")
-    VectorExample()
 }
-fun VectorExample(){
+
+fun VectorExample() {
 
     //Create a vector
     var vec = Vector<String?>()
@@ -20,17 +21,19 @@ fun VectorExample(){
     vec.addElement("Cat");
     vec.addElement("Deer");
 
-    System.out.println("Elements are: "+vec);
+    System.out.println("Elements are: " + vec);
 }
-fun tableauLinkedListJava(){
+
+fun LinkedListExemple() {
     val al2 = LinkedList<String>()
     al2.add("James");//adding object in linkedlist
     al2.add("Serena");
     al2.add("Swati");
     al2.add("Junaid");
-    System.out.println("linkedlist: "+al2);
+    System.out.println("linkedlist: " + al2);
 }
-fun tableauTreeSetJava(){
+
+fun TreeSetExemple() {
     println()
     var al = TreeSet<String?>()
     al.add("Ravi");
@@ -38,11 +41,12 @@ fun tableauTreeSetJava(){
     al.add("Ravi");
     al.add("Ajay");
     var itr: Iterator<String?> = al.iterator()
-    while(itr.hasNext()){
+    while (itr.hasNext()) {
         System.out.println(itr.next());
     }
 }
-fun tableauArrayList(){
+
+fun ArrayListExemple() {
 
     var arrayList = ArrayList<String>()
 
@@ -50,7 +54,7 @@ fun tableauArrayList(){
     arrayList.add("Venus")
 
     println("===> ArrayList <===")
-    for (index in arrayList){
+    for (index in arrayList) {
         println(index)
     }
 
@@ -59,18 +63,20 @@ fun tableauArrayList(){
     for (index in arrayList)
         println(index)
 
-    var arraylist1=ArrayList<String>()
+    var arraylist1 = ArrayList<String>()
     //Ajout de tous les éléments de la liste de arrayList à arrayList1
     println("Elements dans arraylist1 -->")
     arraylist1.addAll(arrayList)
-    for(i in arraylist1)
+    for (i in arraylist1)
         println(i)
 }
-fun tableauListOf(){
+
+fun ListOfExemple() {
     val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
     println(solarSystem)
 
-    for (index in solarSystem){
+    for (index in solarSystem) {
         println(index)
     }
 }
+
