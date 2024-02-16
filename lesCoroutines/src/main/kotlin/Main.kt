f0953e0b742faafca75943c562d8916a2694fbc2
HamidB <ejecteur@yahoo.fr>
2024-02-16 13:56:10 +0100

    update

--- lesCoroutines/src/main/kotlin/Main.kt
+++ lesCoroutines/src/main/kotlin/Main.kt
@@ -1,79 +1,268 @@
 package fr.hamtec
 
 import kotlinx.coroutines.*
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileReader
-import java.io.InputStream
 
 fun main() {
-    GlobalScopelaunchDemo2()
-}
-
-private fun GlobalScopelaunchDemo2() {
-    println("The courtine is starting")
+    println("Start program")
 
     runBlocking {
-        println("Starting runBlocking...")
-        val apiJob = GlobalScope.launch {
-            callAPI()
+        //println("Coroutine runBlocking +++++")
+
+        val parentJob = GlobalScope.launch {
+
+            println("Parent coroutine")
+            launch {
+                println("First child coroutine ")
+                delay(1000)
+                println("End of first child coroutine ")
+            }
+            launch(NonCancellable) {
+                println("Second child coroutine ")
+                delay(1000)
+                println("End of second child coroutine ")
+            }
+            println("End of parent coroutine")
         }
-        GlobalScope.launch{
-            println("Starting GlobalScope.launch...")
-            delay(10_000)
-            println("Finish GlobalScope.launch 10_000...")
+        parentJob.cancel()
+        //println("End coroutine runBlocking +++++")
+    }
+    Thread.sleep(1500)
+    println("End of program")
+
+}
+fun addObjectTypeJob(){
+    println("Start program")
+
+    runBlocking {
+        //println("Coroutine runBlocking +++++")
+
+        val parentJob = GlobalScope.launch {
+
+            println("Parent coroutine")
+            launch {
+                println("First child coroutine ")
+                delay(1000)
+                println("End of first child coroutine ")
+            }
+            launch(NonCancellable) {
+                println("Second child coroutine ")
+                delay(1000)
+                println("End of second child coroutine ")
+            }
+            println("End of parent coroutine")
+        }
+        parentJob.cancel()
+        //println("End coroutine runBlocking +++++")
+    }
+    Thread.sleep(1500)
+    println("End of program")
+}
+
+fun gestionCentralisee() {
+    println("==> Start program")
+
+    runBlocking {
+
+        val parentJob = GlobalScope.launch(CoroutineExceptionHandler { _, throwable ->
+            println("An error occured while executing the coroutine : ${throwable.message}")
+        })
+
+        {
+
+            println("Parent coroutine runBlocking +++++")
+
+            launch {
+                println("First child coroutine ")
+                delay(1000)
+                println("End of first child coroutine ")
+            }
+            launch {
+                println("Second child coroutine ")
+                50 / 0
+                println("End of second child coroutine ")
+            }
+
+
+        }
+        println("End of parent coroutine runBlocking +++++")
+        parentJob.join()
+    }
+
+    println("End of program")
+}
+
+fun coroutineRunCatching() {
+    println("Start program")
+
+    runBlocking {
+        println("Parent coroutine runBlocking +++++")
+
+
+        launch {
+            println("First child coroutine ")
+            delay(1000)
+            println("End of first child coroutine ")
+        }
+        launch {
+            println("Second child coroutine ")
+            delay(1000)
+            val result = runCatching {
+                50 / 0
+            }
+            println("End of second child coroutine with success ? ${result.isSuccess}")
+            println("End of second child coroutine with Failure ? ${result.isFailure}")
+
+            println("End of second child coroutine ")
+        }
+
+        println("End of parent coroutine runBlocking +++++")
+    }
+
+    println("End of program")
+}
+
+fun coroutineTryCath() {
+    println("Start program")
+
+    runBlocking {
+        println("Parent coroutine runBlocking +++++")
+
+
+        launch {
+            println("First child coroutine ")
+            delay(1000)
+            println("End of first child coroutine ")
+        }
+        launch {
+            println("Second child coroutine ")
+            delay(1000)
+            try {
+                50 / 0
+            } catch (exe: Exception) {
+                println("Gestion des exception avec le message : ${exe.message}")
+            }
+
+            println("End of second child coroutine ")
+        }
+
+        println("End of parent coroutine runBlocking +++++")
+    }
+
+
+
+    println("End of program")
+}
+
+fun coroutineParentEnfant() {
+    println("Start program")
+
+    runBlocking {
+        println("Parent coroutine runBlocking +++++")
+
+        val job = GlobalScope.launch {
+            println("Parent coroutine GlobalScope.launch -----")
+
+            launch {
+                println("First child coroutine *+*+*")
+                delay(1000)
+                println("End of first child coroutine *+*+*")
+            }
+            launch {
+                println("Second child coroutine *****")
+                delay(1000)
+                println("End of second child coroutine *****")
+            }
+            println("End of parent coroutine GlobalScope.launch -----")
+        }
+        job.cancel()
+        println("End of parent coroutine après job.cancel() +++++")
+    }
+
+    println("End of program")
+}
+
+fun coroutineDansUnCoroutinev1() {
+    println("Avant la coroutine runBlocking")
+
+    runBlocking {
+        println("Parent coroutine runBlocking")
+        launch {
+            println("First child coroutine")
+            delay(1000)
+            println("End of first child coroutine")
+        }
+        launch {
+            println("Second child coroutine")
+            delay(1000)
+            println("End of second child coroutine")
+        }
+        println("End of parent coroutine runBlocking")
+    }
+
+    println("Après la coroutine runBlocking")
+}
+
+fun coroutineDansUnCoroutinev2() {
+
+}
+
+fun annulerUneCoroutine() {
+    println("Avant la coroutine")
+
+    runBlocking {
+
+        val apiJob = launch {
+            println("Dans la 1er coroutine avant le sleep...")
+            //callAPI()
+            println("Dans la 1er coroutine après le sleep...")
+        }
+        launch {
+            println("Dans la 2éme coroutine avant le sleep et le cancel...")
+            delay(1_000)
             apiJob.cancel()
+            println("Dans la 2éme coroutine après le sleep et cancel...")
         }
         apiJob.join()
         println("The API has been cancelled ? ${apiJob.isCancelled}")
-        println("Finished runBlocking...")
+
     }
+
+    println("Après la coroutine")
 }
-suspend fun callAPI():String {
-    delay(30_000)
+
+suspend fun callAPI(): String {
+    println("Dans la méthode callAPI")
+    delay(10_000)
     return "The result"
 }
 
-private fun runBlockingDemo() {
-    // + le thread principal attend que la coroutine à l’intérieur se termine en premier.
-    // + Il existe des scénarios spécifiques dans lesquels vous souhaiterez peut-être exécuter une
-    // + coroutine et attendre son résultat de manière bloquante. Par exemple, les tests et les fonctions
-    // + principales sont des cas d’utilisation courants.
-    println("Before coroutine")
+fun courtineLaunchJobSimple() {
+    println("Avant la coroutine")
+
+    val job = GlobalScope.launch {
+        println("Dans la coroutine avant le sleep...")
+        delay(3000)
+        println("Dans la coroutine après le sleep...")
+    }
 
     runBlocking {
-        println("In coroutine: Before sleep")
-        Thread.sleep(5_000) // ** 1s
-        println("In coroutine: After sleep")
+        job.join()
     }
 
-    println("After coroutine")
+    println("Après la coroutine")
 }
 
-private fun GlobalScopelaunchDemo() {
+fun coroutineRunBlockingSimple() {
+    println("Avant la coroutine")
 
-    println("Before coroutine")
-    // + Lance une nouvelle coroutine sans bloquer le thread courant et renvoie une référence à la coroutine sous
-    // + la forme d'un job. La coroutine est annulée lorsque le travail résultant est annulé.
-    GlobalScope.launch {
-        println("In coroutine: Before sleep")
-        Thread.sleep(2_000) // ** 5s
-        println("In coroutine: After sleep")
+    runBlocking {
+
+        println("Dans la coroutine... Avant la pause...")
+        delay(2000)
+        println("Dans la coroutine... Après la pause...")
     }
 
-    println("After coroutine + sleep:5")
-    Thread.sleep(5_000) // ** 5s
+    println("Après la couroutine")
 }
 
-fun lectureEcr() {
-    val file = File("/app/logs/january.log")
-    val reader = BufferedReader(FileReader(file, Charsets.UTF_8))
-    reader.lines().forEach { println(it) }
-}
 
-fun lectureDemo() {
-    // ** https://stacklima.com/lire-a-partir-de-fichiers-a-l-aide-de-inputreader-dans-kotlin/
-    val inputStream: InputStream = File("gfg.txt").inputStream()
-    val inputString = inputStream.reader().use { it.readText() }
-    println(inputString)
-}
