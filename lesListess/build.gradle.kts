ca7fd461ab1fda649085ea808201c81aef08aaf8
HamidB <ejecteur@yahoo.fr>
2024-04-24 15:12:24 +0200

    init lesListes

---
+++ lesListess/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "fr.hamtec"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
