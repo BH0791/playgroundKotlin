ecb74d0cd5aead1b8a51242d7d377ede0920cc64
HamidB <ejecteur@yahoo.fr>
2024-08-10 10:12:33 +0200

    Félicitations

---
+++ lesCollections/src/main/kotlin/SupportLibre.kt
@@ -0,0 +1,5 @@
+fun monTab(): Unit {
+    val tab = listOf(1,2,3,4,5,6,7,8,9,10)
+
+    tab.forEach {println(it)}
+}
--- lesListess/src/main/kotlin/Main.kt
+++ lesListess/src/main/kotlin/Main.kt
@@ -6,86 +6,86 @@ fun main() {
 //    presentationDesListesModifiable()
 //    presentationDesListesModifiable()
 //    lireUneBoucleFor()
-    miseEnPratique()
+//    miseEnPratique()
 }
 
-private fun lireUneBoucleFor() {
-    val names = listOf("Jessica", "Henry", "Alicia", "Jose")
-    for(name in names) {
-        println("$name - Number of characters: ${name.length}")
-    }
-}
-
-private fun lireUneBoucleWhile() {
-    val guestsPerFamily = listOf(2, 4, 1, 3)
-    println("$guestsPerFamily")
-    var totalGuests = 0
-    var index = 0
-    while(index < guestsPerFamily.size) {
-        totalGuests += guestsPerFamily[index]
-        index++
-    }
-    println("Total Guest Count: $totalGuests")
-}
-
-private fun presentationDesListesModifiable() {
-    //- MutableList
-    val entrees: MutableList<String> = mutableListOf()
-    println("Add noodles: ${entrees.add("noodles")}")
-    println("Entrees: $entrees")
-    println("Add spaghetti: ${entrees.add("spaghetti")}")
-    println("Entrees: $entrees")
-    println()
-
-    val moreItems = listOf("ravioli", "lasagna", "fettuccine")
-    println("Add list: ${entrees.addAll(moreItems)}")
-    println("Entrees: $entrees")
-    println("Remove spaghetti: ${entrees.remove("spaghetti")}")
-    println("Entrees: $entrees")
-    println("Remove item that doesn't exist: ${entrees.remove("rice")}")
-    println("Entrees: $entrees")
-    println("Remove first element: ${entrees.removeAt(0)}")
-    println("Entrees: $entrees")
-    entrees.clear()
-    println("Entrees: $entrees")
-    println("Empty? ${entrees.isEmpty()}")
-}
-
-private fun presentationDesListes() {
-    val numbers1: List<Int> = listOf(1, 2, 3, 4, 5, 6)
-    val numbers = listOf(1, 2, 3, 4, 5, 6)
-
-    numbers1.forEach {
-        println(it)
-    }
-    println("List: $numbers")
-    println("Size: ${numbers.size}")
-    println("First element: ${numbers[0]}")
-    println("Second element: ${numbers[1]}")
-    println("Last index: ${numbers.size - 1}")
-    println("Last element: ${numbers[numbers.size - 1]}")
-    println("First: ${numbers.first()}")
-    println("Last: ${numbers.last()}")
-    println("Contains 4? ${numbers.contains(4)}")
-    println("Contains 7? ${numbers.contains(7)}")
-    println()
-
-    val colors = listOf("green", "orange", "blue")
-    println("$colors")
-    // ** La fonction reversed() renvoie une nouvelle liste dans laquelle les éléments sont dans l'ordre inverse.
-    println("Reversed list: ${colors.reversed()}")
-    var newList = colors.reversed()
-    println("$newList")
-
-    // ** sorted() renvoie une nouvelle liste dans laquelle les éléments sont triés par ordre croissant.
-    println("Sorted list: ${colors.sorted()}")
-    println()
-
-    val oddNumbers = listOf(5, 3, 7, 1)
-    println("List: $oddNumbers")
-    println("Sorted list: ${oddNumbers.sorted()}")
-}
-
-private fun miseEnPratique() {
-    // ** https://developer.android.com/codelabs/basic-android-kotlin-training-lists?hl=fr#4
-}
+//private fun lireUneBoucleFor() {
+//    val names = listOf("Jessica", "Henry", "Alicia", "Jose")
+//    for(name in names) {
+//        println("$name - Number of characters: ${name.length}")
+//    }
+//}
+//
+//private fun lireUneBoucleWhile() {
+//    val guestsPerFamily = listOf(2, 4, 1, 3)
+//    println("$guestsPerFamily")
+//    var totalGuests = 0
+//    var index = 0
+//    while(index < guestsPerFamily.size) {
+//        totalGuests += guestsPerFamily[index]
+//        index++
+//    }
+//    println("Total Guest Count: $totalGuests")
+//}
+//
+//private fun presentationDesListesModifiable() {
+//    //- MutableList
+//    val entrees: MutableList<String> = mutableListOf()
+//    println("Add noodles: ${entrees.add("noodles")}")
+//    println("Entrees: $entrees")
+//    println("Add spaghetti: ${entrees.add("spaghetti")}")
+//    println("Entrees: $entrees")
+//    println()
+//
+//    val moreItems = listOf("ravioli", "lasagna", "fettuccine")
+//    println("Add list: ${entrees.addAll(moreItems)}")
+//    println("Entrees: $entrees")
+//    println("Remove spaghetti: ${entrees.remove("spaghetti")}")
+//    println("Entrees: $entrees")
+//    println("Remove item that doesn't exist: ${entrees.remove("rice")}")
+//    println("Entrees: $entrees")
+//    println("Remove first element: ${entrees.removeAt(0)}")
+//    println("Entrees: $entrees")
+//    entrees.clear()
+//    println("Entrees: $entrees")
+//    println("Empty? ${entrees.isEmpty()}")
+//}
+//
+//private fun presentationDesListes() {
+//    val numbers1: List<Int> = listOf(1, 2, 3, 4, 5, 6)
+//    val numbers = listOf(1, 2, 3, 4, 5, 6)
+//
+//    numbers1.forEach {
+//        println(it)
+//    }
+//    println("List: $numbers")
+//    println("Size: ${numbers.size}")
+//    println("First element: ${numbers[0]}")
+//    println("Second element: ${numbers[1]}")
+//    println("Last index: ${numbers.size - 1}")
+//    println("Last element: ${numbers[numbers.size - 1]}")
+//    println("First: ${numbers.first()}")
+//    println("Last: ${numbers.last()}")
+//    println("Contains 4? ${numbers.contains(4)}")
+//    println("Contains 7? ${numbers.contains(7)}")
+//    println()
+//
+//    val colors = listOf("green", "orange", "blue")
+//    println("$colors")
+//    // ** La fonction reversed() renvoie une nouvelle liste dans laquelle les éléments sont dans l'ordre inverse.
+//    println("Reversed list: ${colors.reversed()}")
+//    var newList = colors.reversed()
+//    println("$newList")
+//
+//    // ** sorted() renvoie une nouvelle liste dans laquelle les éléments sont triés par ordre croissant.
+//    println("Sorted list: ${colors.sorted()}")
+//    println()
+//
+//    val oddNumbers = listOf(5, 3, 7, 1)
+//    println("List: $oddNumbers")
+//    println("Sorted list: ${oddNumbers.sorted()}")
+//}
+//
+//private fun miseEnPratique() {
+//    // ** https://developer.android.com/codelabs/basic-android-kotlin-training-lists?hl=fr#4
+//}
