ec43dd41eaf0a38e608d6fab960d3e4349691bd6
HamidB <ejecteur@yahoo.fr>
2024-11-11 22:44:29 +0100

    Update

--- lesClasses/src/main/kotlin/fr/hamtec/chatETchien/Dog.kt
+++ lesClasses/src/main/kotlin/fr/hamtec/chatETchien/Dog.kt
@@ -1,7 +1,5 @@
 package fr.hamtec.chatETchien
 
-import kotlin.math.abs
-
 class Dog constructor(
     var name: String,
     var age: Int,
@@ -11,37 +9,6 @@ class Dog constructor(
     var weight: Float,
     var position: Char = 't'
 ) {
-    //--Contrôle de l'age
-    init {
-        if(age in 0..100){
-            age = age
-        } else{
-            age = 55
-        }
-    }
-
-    //-- Méthodes
-
-    fun isSitting(): Boolean = position == 's'
-    fun islaying(): Boolean = position == 'l'
-    fun isStanding(): Boolean = position == 't'
-
-    fun eat(foodWeight: Int){
-        weight += (foodWeight / 1000f)
-    }
-    fun run(distance: Int){
-        weight -= (distance / 1000f) / 1000
-    }
-
-    override fun toString(): String {
-        return "Dog(" +
-                "\n\tname = $name " +
-                "\n\tage = $age " +
-                "\n\trace = $race " +
-                "\n\tcolor = $color " +
-                "\n\tweight = $weight " +
-                "\n\tposition = $position" +
-                "\n)"
-    }
+    constructor(name: String): this(name, 0, "unknown", "unknown", 0, 0f, 't')
 
 }
