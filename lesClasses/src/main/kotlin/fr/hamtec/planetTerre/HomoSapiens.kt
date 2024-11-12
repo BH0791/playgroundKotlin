0f7882db43989bb24145e0314c0713e4ff58dc2a
HamidB <ejecteur@yahoo.fr>
2024-11-12 04:48:44 +0100

    Update

---
+++ lesClasses/src/main/kotlin/fr/hamtec/planetTerre/HomoSapiens.kt
@@ -0,0 +1,12 @@
+package fr.hamtec.planetTerre
+
+class HomoSapiens(
+
+    val name: String,
+    val surname: String,
+    val species: String,
+    val sex: String,
+    val age: Int,
+    val weight: Float,
+    val size: Double
+)
--- lesClasses/src/main/kotlin/fr/hamtec/planetTerre/Person.kt
+++ lesClasses/src/main/kotlin/fr/hamtec/planetTerre/Person.kt
@@ -3,8 +3,10 @@ package fr.hamtec.planetTerre
 class Person(
     val name: String,
     val surname: String,
-    var age: Int
+    var age: Int,
+    var isEmployed: Boolean = true
 ) {
+    //+ Contrôle de l'age entre 0 et 100
     init {
         if (this.age in 0..100) {
             this.age = age
@@ -13,14 +15,10 @@ class Person(
         }
     }
 
-    //    var name = "Name"
-//        set(value) {
-//            field = "Name: $value"
-//        }
-//    var surname = "Surname"
-
+    //-Plus simple
     fun getFullName() = "$name $surname"
 
+    //-Ancienne méthode java
     fun getFullNameAncienne(): String {
         return "$name $surname $age an(s)"
     }
