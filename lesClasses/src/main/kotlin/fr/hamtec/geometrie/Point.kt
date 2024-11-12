b17c96b307097b5d43bc1c2481e51c63c528edee
HamidB <ejecteur@yahoo.fr>
2024-11-12 03:52:37 +0100

    Update

--- lesClasses/src/main/kotlin/fr/hamtec/geometrie/Point.kt
+++ lesClasses/src/main/kotlin/fr/hamtec/geometrie/Point.kt
@@ -1,9 +1,17 @@
 package fr.hamtec.geometrie
 
 class Point(
-    val x: Int,
-    val y: Int
+    var x: Int,
+    var y: Int
 ) {
+    fun deplacement(dx: Int, dy: Int): Unit {
+        x += dx
+        y += dy
+    }
+
+    fun affiche(): Unit {
+        println("Point(x=$x, y=$y)")
+    }
 
     override fun toString(): String {
         return "Point(x=$x, y=$y)"
