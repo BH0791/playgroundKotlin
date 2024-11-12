eb7abdbbd53a77384adc44c129e5970c9609fa94
HamidB <ejecteur@yahoo.fr>
2024-11-12 03:35:35 +0100

    Update

--- lesClasses/src/main/kotlin/fr/hamtec/chatETchien/SupportChap2.kt
+++ lesClasses/src/main/kotlin/fr/hamtec/chatETchien/SupportChap2.kt
@@ -4,5 +4,10 @@ fun utiliserObject(): Unit {
     val minette = Cat("Mioumiou", 2, "Siamois", "green", 110, 7.5f)
     val labrador = Dog("Dogo", 4, "labrador", "brown", 180, 10.5f)
 
-
+    val animal = object : Animal("Animal", 1, "unknown", "unknown", 10, 5f) {
+        override fun eat(foodWeight: Int) {
+            weight += (foodWeight / 1_000f)
+        }
+    }
+    println(animal.toString())
 }
