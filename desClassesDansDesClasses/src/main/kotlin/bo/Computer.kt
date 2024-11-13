28da03e4e6903a311780f6beedc69289344c50fe
HamidB <ejecteur@yahoo.fr>
2024-11-13 18:22:17 +0100

    Update

--- desClassesDansDesClasses/src/main/kotlin/bo/Computer.kt
+++ desClassesDansDesClasses/src/main/kotlin/bo/Computer.kt
@@ -1,14 +1,24 @@
 package fr.hamtec.bo
 
 class Computer(
-    var ramSize: Int,
+    private var ramSize: Int,
     val hardDiskSize: Int,
     val graphicsCardName: String
 ) {
+    fun getComputerInf(): Unit {
+        MotherBoard("55", "Fantasssss").displayComputerInfo()
+        println("==> $ramSize $hardDiskSize $graphicsCardName")
+    }
+
     inner class MotherBoard(val name: String, val model: String) {
         fun displayComputerInfo(): Unit {
+            ramSize = 2024
             println("Computer($ramSize, $hardDiskSize, $graphicsCardName, $name, $model)")
         }
+    }
+
+    inner class HardDisk(val sizeInGb: Int) {
+        fun getInfo() = "Installed on ${this@Computer} with $sizeInGb GB"
     }
 }
 
@@ -17,6 +27,8 @@ fun testInner(): Unit {
     val computerInfo = computer.MotherBoard("GIGABYTE", "GX-299")
 
     computer.MotherBoard("ASUS", "GX-299")
+    println(computer.HardDisk(1000).getInfo())
+    computer.getComputerInf()
 
     computer.MotherBoard("ASUS", "GX-299").displayComputerInfo()
     computerInfo.displayComputerInfo()
